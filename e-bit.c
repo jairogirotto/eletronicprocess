ca6ef4b4248384cada064bebf6a0c3c31aa62e3e
Jairo Girotto <jairogirotto@gmail.com>
2017-11-19 17:28:21 -0200

    bl

---
+++ e-bit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(void)
+{
+    printf("0 & 0 é %d\n", 0 & 0);
+    printf("0 & 1 é %d\n", 0 & 1);
+    printf("1 & 1 é %d\n", 1 & 1);
+    printf("1 & 2 é %d\n", 1 & 2);
+    printf("15 & 127 é %d\n", 15 & 127);
+}
---
+++ inv_bit.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main(void)
+{
+    int valor = 0xFF;
+    printf("O inverso de %X é %X \n",valor,~valor);
+}
